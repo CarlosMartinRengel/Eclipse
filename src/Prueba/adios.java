d7f63590e8b56fcd7b26376a675f0d45b58d9344
AlumnoT <AlumnoT@DESKTOP-L6B4UDC>
2024-10-09 18:04:40 +0200

    Commit adios

--- src/Prueba/adios.java
+++ src/Prueba/adios.java
@@ -1,12 +1,10 @@
 package Prueba;
 
-public class hello {
+public class adios {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		
-		System.out.println("Nueva prueba");
+		System.out.println("Adios");
 	}
 
 }
